b91fd59313437ae4229cdc6c62b3f5b58fd08288
Serg Skol <sps676@gmail.com>
2022-12-13 17:21:04 +0100

    remove eslint-ignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-/* eslint class-methods-use-this: ["error", { "exceptMethods": ["render"] }] */
-/* eslint-env es6 */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -7,26 +5,24 @@ import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={<Home />}
-          />
-          <Route
-            path="/calculator"
-            element={<Calculator />}
-          />
-          <Route
-            path="/quote"
-            element={<Quote />}
-          />
-        </Routes>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
+      <Route
+        path="/calculator"
+        element={<Calculator />}
+      />
+      <Route
+        path="/quote"
+        element={<Quote />}
+      />
+    </Routes>
+  </Router>
+);
+
+export default App;
