4b2b61f1f2b324d6365621fa241ca6797402458e
Serg Skol <sps676@gmail.com>
2022-12-13 16:58:05 +0100

    linters

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => (
   <div className="homeContent">
     <h2>Welcome to our page!</h2>
-    <p>
+    <p className='homeText'>
       This web site was created follow the tutorial:
       React Tutorial From Scratch: A Step-by-Step Guide (2021)!
       https://ibaslogic.com/react-tutorial-for-beginners/
